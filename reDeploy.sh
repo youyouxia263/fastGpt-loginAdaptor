0bf57f1da1fcc4abec8be2aabdc90431391d54b4
youyou263 <645415353@qq.com>
2025-03-02 19:39:30 +0800

    addd re

---
+++ reDeploy.sh
@@ -0,0 +1,7 @@
+IMAGE?=login-page
+TAG:=latest
+npm install
+npm run build
+docker build ./ -t $(IMAGE):$(TAG) --push
+docker rm -f login-page
+docker run -itd -p8080:80 --restart always --name login-page $(IMAGE):$(TAG)
